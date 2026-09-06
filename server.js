8ea4c0757140f3c11400e633c3c0a4021b1b1665
Sheak Raju <rajusheaksulyman@gmail.com>
2026-09-06 19:36:46 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -88,8 +88,7 @@ app.use((req, res, next) => {
 });
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.get('/icons/favicon.ico', (req, res) => res.type('image/x-icon').sendFile(path.join(__dirname, 'icons', 'favicon.ico')));
-app.get(/^\/icons\/icon-\d+x\d+\.png$/, (req, res) => res.type('image/png').sendFile(path.join(__dirname, req.path)));
-app.get('/icons/icon-maskable.svg', (req, res) => res.type('image/svg+xml').sendFile(path.join(__dirname, 'icons', 'icon-maskable.svg')));
+app.get(/^\/icons\/icon-(?:\d+x\d+|maskable)\.png$/, (req, res) => res.type('image/png').sendFile(path.join(__dirname, req.path)));
 app.get(['/tiktok-downloader', '/instagram-downloader', '/web-image-downloader', '/video-url-downloader', '/public-media-downloader'], (req, res) => res.sendFile(path.join(__dirname, 'seo.html')));
 
 function parseUrl(value) {
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'clipgrab-shell-v3';
 const MEDIA_CACHE_NAME = 'clipgrab-media-v1';
-const SHELL = ['/', '/manifest.webmanifest', '/icons/favicon.ico', '/icons/icon-180x180.png', '/icons/icon-192x192.png', '/icons/icon-384x384.png', '/icons/icon-512x512.png', '/icons/icon-maskable.svg'];
+const SHELL = ['/', '/manifest.webmanifest', '/icons/favicon.ico', '/icons/icon-180x180.png', '/icons/icon-192x192.png', '/icons/icon-384x384.png', '/icons/icon-512x512.png', '/icons/icon-maskable.png'];
 let offlineEnabled = false;
 
 self.addEventListener('install', event => {
